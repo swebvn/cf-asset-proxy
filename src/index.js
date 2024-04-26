ce1e1fde65a80a82737ed0291e2e1f306e0b4b31
bangnokia <bangnokia@gmail.com>
2024-04-26 16:17:12 +0700

    add cache header

--- src/index.js
+++ src/index.js
@@ -38,6 +38,7 @@ function modifyResponseHeaders(originalHeaders) {
     headers.set('Access-Control-Allow-Origin', '*')
     headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
     headers.set('Access-Control-Allow-Headers', 'Content-Type')
+    headers.set('Cache-Control', 'public, max-age=600000')
 
     return headers
 }
